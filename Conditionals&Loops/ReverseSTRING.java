dfe8a477c9ac82aa37d0387ff0bc2ae8992250e8
Pratyush <pratyushkumarsinha798@gmail.com>
2023-06-15 23:22:12 +0530

    Reverse a string code added

--- Conditionals&Loops/ReverseSTRING.java
+++ Conditionals&Loops/ReverseSTRING.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class ReverseSTRING {
     public static void main(String[] args) {
-
+        Scanner in= new Scanner(System.in);
+        System.out.print("Enter a string to reverse: ");
+        String str=in.nextLine();
+        String revstr ="";
+        for (int i=0; i<str.length(); i++){
+            revstr=str.charAt(i)+revstr;
+        }
+        System.out.println("Reversed string is: "+revstr);
     }
 }
