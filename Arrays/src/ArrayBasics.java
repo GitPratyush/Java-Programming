b3c872792d7bf8ed7dfc72b534f81db6feb85a7f
Pratyush <pratyushkumarsinha798@gmail.com>
2023-06-20 20:44:22 +0530

    Added Arrays

---
+++ Arrays/src/ArrayBasics.java
@@ -0,0 +1,9 @@
+public class ArrayBasics {
+    public static void main(String[] args) {
+        //Syntax
+        //datatype[] variable_name=new datatype[size];
+        int[] rollnos=new int[6];   //storing 6 roll numbers which is of integer datatype.
+        //or we can also use:-
+        int[] rollnumber={1,3,4,6,7,21};
+    }
+}
