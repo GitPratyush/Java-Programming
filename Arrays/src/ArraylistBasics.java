8e0fa9eb1e6a50aa3c5d02d9fef027c112edd075
Pratyush <pratyushkumarsinha798@gmail.com>
2023-06-21 20:36:27 +0530

    Updated Array

---
+++ Arrays/src/ArraylistBasics.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class ArraylistBasics {
+    public static void main(String[] args) {
+        //Syntax
+        ArrayList<Integer> list=new ArrayList<>();
+
+    }
+}
